f056d7bd0cbe096bec1e8bac84e059f5226f04f3
sachahjkl <sacha@sacha.house>
2026-06-14 17:27:01 +0200

    change sops usb key to be a hardcoded default name, instead of dynamic per host

--- modules/secrets.mod.nix
+++ modules/secrets.mod.nix
@@ -4,14 +4,21 @@
     lib,
     ...
   }: let
-    inherit (lib) mkDefault mkIf mkMerge;
+    inherit (lib) mkDefault mkIf mkMerge mkOption;
     inherit (lib.modules) mkAliasOptionModule;
+    inherit (lib.types) str;
     hasMediaKeyIdentity = builtins.any (path: lib.hasPrefix "/media/key/" path) config.age.identityPaths;
   in {
     imports = [
       inputs.agenix.nixosModules.default
       (mkAliasOptionModule ["secrets"] ["age" "secrets"])
     ];
+
+    options.preferences.secrets.mediaKeyLabel = mkOption {
+      type = str;
+      default = "sopskey";
+      description = "Filesystem label of the USB media key used for age identities at boot.";
+    };
 
     config = mkMerge [
       {
@@ -26,7 +33,7 @@
         ];
 
         fileSystems."/media/key" = {
-          device = "/dev/disk/by-label/${config.networking.hostName}.s";
+          device = "/dev/disk/by-label/${config.preferences.secrets.mediaKeyLabel}";
           fsType = "exfat";
           options = [
             "ro"
