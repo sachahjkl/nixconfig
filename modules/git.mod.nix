7af34aa454a7ca7ef4436656018ad2110cc4b680
sachahjkl <sacha@sacha.house>
2026-06-15 20:30:58 +0000

    autogenerate authorized_signers file from ssh pubkeys

--- modules/git.mod.nix
+++ modules/git.mod.nix
@@ -1,4 +1,4 @@
-_: {
+{self, ...}: {
   flake.nixosModules.git = {
     config,
     lib,
@@ -46,71 +46,75 @@ _: {
       }
 
       (lib.optionalAttrs hasHjemUsers {
-        hjem.users.${config.userName}.rum.programs.git = {
-          enable = true;
-          package = null;
-          settings = {
-            alias = {
-              oops = "commit --amend --no-edit";
-              pl = "push --force-with-lease";
-              lg = "log --graph --oneline";
-              br = "!git switch $(git branch | grep -v \"^\\*\" | fzf)";
-              latest = "!git switch \"$1\" && git pull origin $1 #";
-              echo = "!echo - $1 - #";
-            };
-            column.ui = "auto";
-            branch.sort = "-committerdate";
-            tag = {
-              sort = "version:refname";
-              gpgSign = true;
-            };
-            init.defaultBranch = "master";
-            diff = {
-              algorithm = "histogram";
-              colorMoved = "plain";
-              mnemonicPrefix = true;
-              renames = true;
-            };
-            push = {
-              default = "simple";
-              autoSetupRemote = true;
-              followTags = true;
-            };
-            fetch = {
-              prune = true;
-              pruneTags = true;
-              all = true;
-            };
-            help.autocorrect = "prompt";
-            commit = {
-              verbose = true;
-              gpgSign = true;
-            };
-            rerere.autoupdate = true;
-            core = {
-              excludesfile = "~/.gitignore";
-              fsmonitor = true;
-              untrackedCache = true;
-            };
-            rebase = {
-              autoSquash = true;
-              autoStash = true;
-              updateRefs = true;
-            };
-            pull.rebase = true;
-            http.sslVerify = true;
-            merge.tool = "mergiraf";
-            mergetool.keepBackup = false;
-            "mergetool \"mergiraf\"" = {
-              cmd = "mergiraf --mode=merge3 --base=\"$BASE\" --left=\"$LOCAL\" --right=\"$REMOTE\" --output=\"$MERGED\"";
-              trustExitCode = true;
-            };
-            gpg.format = "ssh";
-            "gpg \"ssh\"".allowedSignersFile = "~/.ssh/allowed_signers";
-            user = {
-              name = cfg.authorName;
-              email = cfg.authorEmail;
-              inherit (cfg) signingKey;
+        hjem.users.${config.userName} = {
+          files.".ssh/allowed_signers".text = lib.concatMapStrings (key: "${cfg.authorEmail} ${key}\n") self.keys-admin;
+
+          rum.programs.git = {
+            enable = true;
+            package = null;
+            settings = {
+              alias = {
+                oops = "commit --amend --no-edit";
+                pl = "push --force-with-lease";
+                lg = "log --graph --oneline";
+                br = "!git switch $(git branch | grep -v \"^\\*\" | fzf)";
+                latest = "!git switch \"$1\" && git pull origin $1 #";
+                echo = "!echo - $1 - #";
+              };
+              column.ui = "auto";
+              branch.sort = "-committerdate";
+              tag = {
+                sort = "version:refname";
+                gpgSign = true;
+              };
+              init.defaultBranch = "master";
+              diff = {
+                algorithm = "histogram";
+                colorMoved = "plain";
+                mnemonicPrefix = true;
+                renames = true;
+              };
+              push = {
+                default = "simple";
+                autoSetupRemote = true;
+                followTags = true;
+              };
+              fetch = {
+                prune = true;
+                pruneTags = true;
+                all = true;
+              };
+              help.autocorrect = "prompt";
+              commit = {
+                verbose = true;
+                gpgSign = true;
+              };
+              rerere.autoupdate = true;
+              core = {
+                excludesfile = "~/.gitignore";
+                fsmonitor = true;
+                untrackedCache = true;
+              };
+              rebase = {
+                autoSquash = true;
+                autoStash = true;
+                updateRefs = true;
+              };
+              pull.rebase = true;
+              http.sslVerify = true;
+              merge.tool = "mergiraf";
+              mergetool.keepBackup = false;
+              "mergetool \"mergiraf\"" = {
+                cmd = "mergiraf --mode=merge3 --base=\"$BASE\" --left=\"$LOCAL\" --right=\"$REMOTE\" --output=\"$MERGED\"";
+                trustExitCode = true;
+              };
+              gpg.format = "ssh";
+              "gpg \"ssh\"".allowedSignersFile = "~/.ssh/allowed_signers";
+              user = {
+                name = cfg.authorName;
+                email = cfg.authorEmail;
+                inherit (cfg) signingKey;
+              };
             };
           };
         };
