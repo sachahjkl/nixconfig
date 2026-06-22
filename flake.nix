bc3be5cc00916559489e7c58f1db0dddf631aa8f
sachahjkl <sacha@sacha.house>
2026-06-22 12:25:06 +0200

    add sachahjkl cachix cache

--- flake.nix
+++ flake.nix
@@ -13,11 +13,13 @@
       "https://hyprland.cachix.org"
       "https://cache.numtide.com"
       "https://nix-community.cachix.org"
+      "https://sachahjkl.cachix.org"
     ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "sachahjkl.cachix.org-1:cepX7PCUV88hCchnh9prZM5V72wRkCf6oSJL6JfgWs0="
     ];
     flake-registry = "";
     http-connections = 50;
