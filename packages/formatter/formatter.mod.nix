01933f3ff3cb026de07a206597a27012c3170997
sachahjkl <sacha@sacha.house>
2026-09-11 22:44:43 +0200

    Use Alejandra directly

--- packages/formatter/formatter.mod.nix
+++ packages/formatter/formatter.mod.nix
@@ -1,15 +1,5 @@
 _: {
   perSystem = {pkgs, ...}: {
-    formatter = pkgs.writeShellApplication {
-      name = "format-nix";
-      runtimeInputs = [pkgs.alejandra];
-      text = ''
-        if (( $# == 0 )); then
-          set -- .
-        fi
-
-        exec alejandra "$@"
-      '';
-    };
+    formatter = pkgs.alejandra;
   };
 }
